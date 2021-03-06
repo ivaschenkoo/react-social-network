e8a3bb35c6869d91b6cf9a6996012fd4bdfb5a4e
Vladislav <vl.ivaschenkoo@gmail.com>
2021-03-06 10:38:37 +0200

    refactored userComponent

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import * as axios from "axios";
+import Users from "./Users";
 import {connect} from "react-redux";
-import UsersAPI from "./UsersAPI";
 import {friendToggleAC, setCurrentPageAC, setUsersAC, setUsersCountAC} from "../../redux/userReducer";
 
+
+class UsersContainer extends React.Component {
+
+    componentDidMount() {
+        this.changeUsers()
+    }
+
+    changeUsers(currentPage=this.props.currentPage, usersCount=10) {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?count=${usersCount}&page=${currentPage}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.updateAllUsersCount(response.data['totalCount']);
+            })
+    }
+
+    changePage(page) {
+        this.props.setPage(page)
+        this.changeUsers(page)
+    }
+
+    render() {
+        return <Users users={this.props.users}
+                      followToggle={this.props.followToggle}
+                      countOfPages={this.props.countOfPages}
+                      currentPage={this.props.currentPage}
+                      changePage={this.changePage.bind(this)} />
+    }
+
+}
 
 let mapStateToProps = (state) => {
     return {
@@ -31,6 +61,5 @@ let mapDispatchToProps = (dispatch) => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(UsersAPI);
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
 
-export default UsersContainer;
